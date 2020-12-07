e84a7386635a3ab46d06aa729190fb793189db53
Potato187 <dhtai97@gmai.com>
2020-12-07 14:11:39 +0700

    fix ts-styled-plugin

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -10,8 +10,7 @@ export const Background = styled.div`
       rgba(0, 0, 0, 0.1),
       rgba(0, 0, 0, 0.35)
     ),
-    url(${({ src }) =>
-        src ? `../images/misc/${src}.jpg` : "../images/misc/home-bg.jpg"})
+    url(${({ src }) => src ? `../images/misc/${src}.jpg` : "../images/misc/home-bg.jpg"})
       top left/cover no-repeat;
 `;
 
